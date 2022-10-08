871dc7f14c754ff5c65b4ca8f9c96e94c6d729ca
vincent porte <vincent@neuralia.co>
2022-10-08 19:43:07 +0200

    add django-extensions and django-debug-toolbar in dev

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,4 +1,4 @@
-from .base import *  # pylint: disable=wildcard-import,unused-wildcard-import
+from .base import *  # pylint: disable=wildcard-import,unused-wildcard-import # noqa F403
 
 # Django settings
 # ---------------
@@ -6,6 +6,20 @@ from .base import *  # pylint: disable=wildcard-import,unused-wildcard-import
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "192.168.0.1"]
+
+INSTALLED_APPS += ["django_extensions", "debug_toolbar"]  # noqa F405
+
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+
+DEBUG_TOOLBAR_CONFIG = {
+    # https://django-debug-toolbar.readthedocs.io/en/latest/panels.html#panels
+    "DISABLE_PANELS": [
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+        # ProfilingPanel makes the django admin extremely slow...
+        "debug_toolbar.panels.profiling.ProfilingPanel",
+    ],
+    "SHOW_TEMPLATE_CONTEXT": True,
+}
 
 # Security.
 # ------------------------------------------------------------------------------
