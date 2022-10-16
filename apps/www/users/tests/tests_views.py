c090f1dd0e293940f7f394e28778669d0b01d4c9
vincent porte <vincent@neuralia.co>
2022-10-16 21:48:26 +0200

    feat(users): add reset password workflow (urls, tests, templates)

--- apps/www/users/tests/tests_views.py
+++ apps/www/users/tests/tests_views.py
@@ -1,9 +1,12 @@
 import uuid
 from datetime import date
 
+from django.contrib.auth.tokens import default_token_generator
 from django.core import mail
 from django.test import TestCase
 from django.urls import reverse
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode
 
 from apps.users.factories import (
     DEFAULT_PASSWORD,
@@ -209,6 +212,110 @@ class UpdatePasswordViewTest(TestCase):
         }
         response = self.client.post(self.url, data=form_data)
         self.assertRedirects(response, self.user.get_absolute_url(), status_code=302)
+
+
+class PasswordResetViewTest(TestCase):
+    def setUp(self) -> None:
+        self.user = UserFactory()
+        self.url = reverse("users:password_reset")
+
+    def test_csrf(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "csrfmiddlewaretoken")
+
+    def test_email_sent(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+        form_data = {
+            "email": self.user.email,
+        }
+        response = self.client.post(self.url, data=form_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response.url,
+            reverse("users:password_reset_done"),
+        )
+
+        self.assertEqual(1, len(mail.outbox))
+        email = mail.outbox[0]
+
+        token = response.context.get("token")
+        uid = response.context.get("uid")
+        password_reset_token_url = reverse("users:password_reset_confirm", kwargs={"uidb64": uid, "token": token})
+        self.assertIn(password_reset_token_url, email.body)
+        self.assertEqual(
+            [
+                self.user.email,
+            ],
+            email.to,
+        )
+
+    def test_unknown_user(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+
+        form_data = {
+            "email": "unknown.email@neuralia.co",
+        }
+        response = self.client.post(self.url, data=form_data)
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(
+            response.url,
+            reverse("users:password_reset_done"),
+        )
+
+        self.assertEqual(0, len(mail.outbox))
+
+
+class PasswordResetDoneViewTest(TestCase):
+    def test_rendered_page(self):
+        response = self.client.get(reverse("users:password_reset_done"))
+        self.assertEqual(response.status_code, 200)
+
+
+class PasswordResetConfirmViewTest(TestCase):
+    def setUp(self):
+        self.user = UserFactory()
+        self.uid = urlsafe_base64_encode(force_bytes(self.user.pk))  # .decode()
+        self.token = default_token_generator.make_token(self.user)
+        self.url = reverse("users:password_reset_confirm", kwargs={"uidb64": self.uid, "token": self.token})
+
+    def test_csrf(self):
+        response = self.client.get(self.url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "csrfmiddlewaretoken")
+
+    def test_valid_token(self):
+        response = self.client.get(self.url, follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        form_data = {
+            "new_password1": DEFAULT_PASSWORD + "1",
+            "new_password2": DEFAULT_PASSWORD + "1",
+        }
+        response = self.client.post(self.url, data=form_data)
+        self.assertEqual(response.status_code, 302)
+        self.user.refresh_from_db()
+
+    def test_invalid_token(self):
+        """
+        invalidate the token by changing the password
+        """
+        self.user.set_password("abcdef123")
+        self.user.save()
+
+        response = self.client.get(self.url, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "reset link was invalid")
+
+
+class PasswordResetCompleteViewTest(TestCase):
+    def test_rendered_page(self):
+        response = self.client.get(reverse("users:password_reset_complete"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, reverse("users:login"))
 
 
 class complete_verificationTest(TestCase):
