fe871fd93a2ca66b1685da0f983da498bd0944dd
vincent porte <vincent@neuralia.co>
2022-10-16 21:48:26 +0200

    feat(users): add reset password workflow (urls, tests, templates)

--- apps/www/users/urls.py
+++ apps/www/users/urls.py
@@ -1,4 +1,5 @@
-from django.urls import path
+from django.contrib.auth.urls import views as auth_views
+from django.urls import path, reverse_lazy
 
 from apps.www.users.views import (
     HostView,
@@ -24,4 +25,32 @@ urlpatterns = [
     path("update-password/", UpdatePasswordView.as_view(), name="password"),
     path("profile/<slug:slug>/", UserProfileView.as_view(), name="profile"),
     path("switch-language/", switch_language, name="switch-language"),
+    path(
+        "reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="users/password_reset.html",
+            email_template_name="users/password_reset_email.html",
+            subject_template_name="users/password_reset_subject.txt",
+            success_url=reverse_lazy("users:password_reset_done"),
+        ),
+        name="password_reset",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetDoneView.as_view(template_name="users/password_reset_done.html"),
+        name="password_reset_done",
+    ),
+    path(
+        "reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="users/password_reset_confirm.html",
+            success_url=reverse_lazy("users:password_reset_complete"),
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/complete/",
+        auth_views.PasswordResetCompleteView.as_view(template_name="users/password_reset_complete.html"),
+        name="password_reset_complete",
+    ),
 ]
